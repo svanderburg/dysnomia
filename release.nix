1d33dd22a17c4bc3b23f3ea775b989c455efd75b
Sander van der Burg <svanderburg@gmail.com>
2013-06-24 17:51:14 +0200

    activation-scripts -> dysnomia

--- release.nix
+++ release.nix
@@ -3,19 +3,19 @@
 let
   jobs = rec {
     tarball =
-      { disnix_activation_scripts ? {outPath = ./.; rev = 1234;}
+      { dysnomia ? {outPath = ./.; rev = 1234;}
       , officialRelease ? false
       }:
 
       with import nixpkgs {};
 
       releaseTools.sourceTarball {
-        name = "disnix-activation-scripts-tarball";
+        name = "dysnomia-tarball";
         version = builtins.readFile ./version;
-        src = disnix_activation_scripts;
+        src = dysnomia;
         inherit officialRelease;
 
-        buildInputs = [ ];
+        buildInputs = [];
       };
 
     build =
@@ -34,7 +34,7 @@ let
       with import nixpkgs { inherit system; };
 
       releaseTools.nixBuild {
-        name = "disnix-activation-scripts";
+        name = "dysnomia";
         src = tarball;
         
         preConfigure =
@@ -42,22 +42,21 @@ let
             ${stdenv.lib.optionalString enableEjabberdDump "export PATH=$PATH:${ejabberd}/sbin"}
           '';
 
-        configureFlags = 
-          ''
-            ${if enableApacheWebApplication then "--with-apache" else "--without-apache"}
-            ${if enableAxis2WebService then "--with-axis2" else "--without-axis2"}
-            ${if enableEjabberdDump then "--with-ejabberd" else "--without-ejabberd"}
-            ${if enableMySQLDatabase then "--with-mysql" else "--without-mysql"}
-            ${if enablePostgreSQLDatabase then "--with-postgresql" else "--without-postgresql"}
-            ${if enableTomcatWebApplication then "--with-tomcat=${catalinaBaseDir}" else "--without-tomcat"}
-            ${if enableSubversionRepository then "--with-subversion" else "--without-subversion"}
-          '';
-
+        configureFlags = ''
+          ${if enableApacheWebApplication then "--with-apache" else "--without-apache"}
+          ${if enableAxis2WebService then "--with-axis2" else "--without-axis2"}
+          ${if enableEjabberdDump then "--with-ejabberd" else "--without-ejabberd"}
+          ${if enableMySQLDatabase then "--with-mysql" else "--without-mysql"}
+          ${if enablePostgreSQLDatabase then "--with-postgresql" else "--without-postgresql"}
+          ${if enableTomcatWebApplication then "--with-tomcat=${catalinaBaseDir}" else "--without-tomcat"}
+          ${if enableSubversionRepository then "--with-subversion" else "--without-subversion"}
+        '';
+        
         buildInputs = []
-                      ++ stdenv.lib.optional enableEjabberdDump ejabberd
-                      ++ stdenv.lib.optional enableMySQLDatabase mysql
-                      ++ stdenv.lib.optional enablePostgreSQLDatabase postgresql
-                      ++ stdenv.lib.optional enableSubversionRepository subversion;
+          ++ stdenv.lib.optional enableEjabberdDump ejabberd
+          ++ stdenv.lib.optional enableMySQLDatabase mysql
+          ++ stdenv.lib.optional enablePostgreSQLDatabase postgresql
+          ++ stdenv.lib.optional enableSubversionRepository subversion;
       };
       
       tests = 
@@ -66,7 +65,7 @@ let
         with import nixpkgs {};
         
         let
-          disnix_activation_scripts = build {
+          dysnomia = build {
             system = "x86_64-linux";
             enableApacheWebApplication = true;
             enableAxis2WebService = true;
@@ -141,7 +140,7 @@ let
                 services.tomcat.enable = true;
                 services.tomcat.axis2.enable = true;
                 
-                environment.systemPackages = [ disnix_activation_scripts ];
+                environment.systemPackages = [ dysnomia ];
               };
             };
             testScript =
@@ -150,8 +149,8 @@ let
                 
                 # Test echo activation script. Here we just invoke the activate
                 # and deactivation steps. This test should succeed.
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/echo activate hello");
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/echo deactivate hello");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/echo activate hello");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/echo deactivate hello");
                 
                 # Test wrapper activation script. Here we invoke the wrapper
                 # of a certain service. This service spawns a process which
@@ -159,9 +158,9 @@ let
                 # After a while we deactivate it and we check if it is indeed
                 # deactivated. This test should succeed.
                 
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/wrapper activate ${wrapper}");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/wrapper activate ${wrapper}");
                 $machine->mustSucceed("[ \"\$(pgrep -f ${wrapper}/bin/loop)\" != \"\" ]");
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/wrapper deactivate ${wrapper}");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/wrapper deactivate ${wrapper}");
                 $machine->mustSucceed("[ \"\$(pgrep -f ${wrapper}/bin/loop)\" = \"\" ]");
                 
                 # Test process activation script. Here we start a process which
@@ -169,10 +168,10 @@ let
                 # then we deactivate it again and verify whether it has been
                 # stopped. This test should succeed.
                 
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/process activate ${process}");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/process activate ${process}");
                 $machine->mustSucceed("sleep 5");
                 $machine->mustSucceed("[ \"\$(pgrep -f ${process}/bin/loop)\" != \"\" ]");
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/process deactivate ${process}");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/process deactivate ${process}");
                 $machine->mustSucceed("sleep 5");
                 $machine->mustSucceed("[ \"\$(pgrep -f ${process}/bin/loop)\" = \"\" ]");
                 
@@ -183,16 +182,16 @@ let
                 # This test should succeed.
                 
                 $machine->waitForJob("httpd");
-                $machine->mustSucceed("documentRoot=/var/www ${disnix_activation_scripts}/libexec/disnix/activation-scripts/apache-webapplication activate ${apache_webapplication}");
+                $machine->mustSucceed("documentRoot=/var/www ${dysnomia}/libexec/dysnomia/apache-webapplication activate ${apache_webapplication}");
                 $machine->mustSucceed("curl --fail http://localhost/test");
-                $machine->mustSucceed("documentRoot=/var/www ${disnix_activation_scripts}/libexec/disnix/activation-scripts/apache-webapplication deactivate ${apache_webapplication}");
+                $machine->mustSucceed("documentRoot=/var/www ${dysnomia}/libexec/dysnomia/apache-webapplication deactivate ${apache_webapplication}");
                 $machine->mustFail("curl --fail http://localhost/test");
                 
                 # Test MySQL activation script. Here we activate a database and
                 # we check whether it is created. This test should succeed.
                 
                 $machine->waitForJob("mysql");
-                $machine->mustSucceed("mysqlUsername=root mysqlPassword=verysecret ${disnix_activation_scripts}/libexec/disnix/activation-scripts/mysql-database activate ${mysql_database}");
+                $machine->mustSucceed("mysqlUsername=root mysqlPassword=verysecret ${dysnomia}/libexec/dysnomia/mysql-database activate ${mysql_database}");
                 my $result = $machine->mustSucceed("echo 'select * from test' | mysql --user=root --password=verysecret -N testdb");
                 
                 if($result =~ /Hello world/) {
@@ -201,14 +200,14 @@ let
                     die "MySQL table should contain: Hello world!\n";
                 }
                 
-                $machine->mustSucceed("mysqlUsername=root mysqlPassword=verysecret ${disnix_activation_scripts}/libexec/disnix/activation-scripts/mysql-database deactivate ${mysql_database}");
+                $machine->mustSucceed("mysqlUsername=root mysqlPassword=verysecret ${dysnomia}/libexec/dysnomia/mysql-database deactivate ${mysql_database}");
                 
                 # Test PostgreSQL activation script. Here we activate a database
                 # and we check whether it is created. This test should succeed.
                 
                 $machine->waitForJob("postgresql");
-                $machine->mustSucceed("postgresqlUsername=root ${disnix_activation_scripts}/libexec/disnix/activation-scripts/postgresql-database activate ${postgresql_database}");
-                my $result = $machine->mustSucceed("echo 'select * from test' | psql --file - testdb");         
+                $machine->mustSucceed("postgresqlUsername=root ${dysnomia}/libexec/dysnomia/postgresql-database activate ${postgresql_database}");
+                my $result = $machine->mustSucceed("echo 'select * from test' | psql --file - testdb");
                 
                 if($result =~ /Hello world/) {
                     print "PostgreSQL query returns: Hello world!\n";
@@ -216,7 +215,7 @@ let
                     die "PostgreSQL table should contain: Hello world!\n";
                 }
                 
-                $machine->mustSucceed("postgresqlUsername=root ${disnix_activation_scripts}/libexec/disnix/activation-scripts/postgresql-database deactivate ${postgresql_database}");
+                $machine->mustSucceed("postgresqlUsername=root ${dysnomia}/libexec/dysnomia/postgresql-database deactivate ${postgresql_database}");
                 
                 # Test Tomcat web application script. Deploys a tomcat web
                 # application, verifies whether it can be accessed and then
@@ -224,19 +223,19 @@ let
                 # This test should succeed.
                 
                 $machine->waitForJob("tomcat");
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/tomcat-webapplication activate ${tomcat_webapplication}");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/tomcat-webapplication activate ${tomcat_webapplication}");
                 $machine->waitForFile("/var/tomcat/webapps/tomcat-webapplication");
                 $machine->mustSucceed("curl --fail http://localhost:8080/tomcat-webapplication");
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/tomcat-webapplication deactivate ${tomcat_webapplication}");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/tomcat-webapplication deactivate ${tomcat_webapplication}");
                 $machine->mustSucceed("while [ -e /var/tomcat/webapps/tomcat-webapplication ]; do echo 'Waiting to undeploy' >&2; sleep 1; done");
                 $machine->mustFail("curl --fail http://localhost:8080/tomcat-webapplication");
 
                 # Test Axis2 web service script.
                 
                 $machine->waitForFile("/var/tomcat/webapps/axis2");
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/axis2-webservice activate ${axis2_webservice}");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/axis2-webservice activate ${axis2_webservice}");
                 $machine->mustSucceed("sleep 10; curl --fail http://localhost:8080/axis2/services/Test/test"); # !!! We must wait a while to let it become active
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/axis2-webservice deactivate ${axis2_webservice}");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/axis2-webservice deactivate ${axis2_webservice}");
                 $machine->mustFail("sleep 10; curl --fail http://localhost:8080/axis2/services/Test/test"); # !!! We must wait a while to let it become inactive
 
                 # Test ejabberd dump activation script. First we check if we can
@@ -246,23 +245,23 @@ let
                 
                 $machine->waitForJob("ejabberd");
                 $machine->mustFail("sleep 3; curl --fail --user admin:admin http://localhost:5280/admin"); # !!! We need to wait for a while even though ejabberd is running
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/ejabberd-dump activate ${ejabberd_dump}");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/ejabberd-dump activate ${ejabberd_dump}");
                 $machine->mustSucceed("curl --fail --user admin:admin http://localhost:5280/admin");
-                $machine->mustSucceed("${disnix_activation_scripts}/libexec/disnix/activation-scripts/ejabberd-dump deactivate ${ejabberd_dump}");
+                $machine->mustSucceed("${dysnomia}/libexec/dysnomia/ejabberd-dump deactivate ${ejabberd_dump}");
                 
                 # Test subversion activation script. We import a repository
                 # then we do a checkout and see whether it succeeds.
                 # This test should succeed.
                 
-                $machine->mustSucceed("svnBaseDir=/repos svnGroup=users ${disnix_activation_scripts}/libexec/disnix/activation-scripts/subversion-repository activate ${subversion_repository}");
+                $machine->mustSucceed("svnBaseDir=/repos svnGroup=users ${dysnomia}/libexec/dysnomia/subversion-repository activate ${subversion_repository}");
                 $machine->mustSucceed("${subversion}/bin/svn co file:///repos/testrepos");
                 $machine->mustSucceed("[ -e testrepos/index.php ]");
-                $machine->mustSucceed("svnBaseDir=/repos svnGroup=users ${disnix_activation_scripts}/libexec/disnix/activation-scripts/subversion-repository deactivate ${subversion_repository}");
+                $machine->mustSucceed("svnBaseDir=/repos svnGroup=users ${dysnomia}/libexec/dysnomia/subversion-repository deactivate ${subversion_repository}");
                 
                 # Test NixOS configuration activation script. We activate the current
                 # NixOS configuration
                 
-                $machine->mustSucceed("disableNixOSSystemProfile=1 testNixOS=1 ${disnix_activation_scripts}/libexec/disnix/activation-scripts/nixos-configuration activate /var/run/current-system");
+                $machine->mustSucceed("disableNixOSSystemProfile=1 testNixOS=1 ${dysnomia}/libexec/dysnomia/nixos-configuration activate /var/run/current-system");
               '';
           };
         };
