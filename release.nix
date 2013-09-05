976bd08619d1bdcca0ec2f41157e0d3a97b5ab51
Sander van der Burg <svanderburg@gmail.com>
2013-09-05 16:09:49 +0200

    Add simple testcase for the command-line utility

--- release.nix
+++ release.nix
@@ -264,6 +264,49 @@ let
                 $machine->mustSucceed("disableNixOSSystemProfile=1 testNixOS=1 ${dysnomia}/libexec/dysnomia/nixos-configuration activate /var/run/current-system");
               '';
           };
-        };
+          
+          frontend = 
+            let
+              mysql_container = writeTextFile {
+                name = "mysql-container";
+                text = ''
+                  type=mysql-database
+                  mysqlUsername=root
+                  mysqlPassword=verysecret
+                '';
+              };
+            in
+            simpleTest {
+              nodes = {
+                machine = {config, pkgs, ...}:
+              
+                {
+                  services.mysql = {
+                    enable = true;
+                    rootPassword = pkgs.writeTextFile { name = "mysqlpw"; text = "verysecret"; };
+                  };
+
+                  environment.systemPackages = [ dysnomia ];
+                };
+              };
+              testScript =
+                ''
+                  startAll;
+                  
+                  # Test MySQL activation script. Here we activate a database and
+                  # we check whether it is created. This test should succeed.
+                
+                  $machine->waitForJob("mysql");
+                  $machine->mustSucceed("dysnomia --operation activate --component ${mysql_database} --container ${mysql_container}");
+                  my $result = $machine->mustSucceed("echo 'select * from test' | mysql --user=root --password=verysecret -N testdb");
+                
+                  if($result =~ /Hello world/) {
+                      print "MySQL query returns: Hello world!\n";
+                  } else {
+                      die "MySQL table should contain: Hello world!\n";
+                  }
+                '';
+          };
+    };
   };
 in jobs
