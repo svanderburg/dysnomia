ad54dd03ad18f72db080d375e3d526d1125cd674
Sander van der Burg <svanderburg@gmail.com>
2016-04-10 20:43:59 +0000

    Test unprivileged processes for idempotence

--- tests/processes-systemd.nix
+++ tests/processes-systemd.nix
@@ -97,6 +97,11 @@ makeTest {
       $machine->mustSucceed("sleep 5; [ \"\$(cat /tmp/wrapper.state)\" = \"wrapper active\" ]");
       $machine->mustSucceed("[ \"\$(stat -c %U /tmp/wrapper.state)\" = \"unprivileged\" ]");
       
+      # Activate again. This test should succeed as it is idempotent.
+      $machine->mustSucceed("dysnomia --type wrapper --operation activate --component ${wrapper_unprivileged} --environment");
+      $machine->mustSucceed("sleep 5; [ \"\$(cat /tmp/wrapper.state)\" = \"wrapper active\" ]");
+      $machine->mustSucceed("[ \"\$(stat -c %U /tmp/wrapper.state)\" = \"unprivileged\" ]");
+      
       # Test wrapper activation script. Here we invoke the lock
       # operation of a certain service. It should write a lock file
       # into the temp dir and it should be owned by an unprivileged user.
@@ -115,7 +120,10 @@ makeTest {
       # Deactivate the wrapper script. We also check whether the file created
       # on activation is owned by the unprivileged user.
       # This test should succeed.
+      $machine->mustSucceed("dysnomia --type wrapper --operation deactivate --component ${wrapper_unprivileged} --environment");
+      $machine->mustSucceed("sleep 5; [ ! -f /tmp/wrapper.state ]");
       
+      # Deactivate again. This test should succeed as it is idempotent.
       $machine->mustSucceed("dysnomia --type wrapper --operation deactivate --component ${wrapper_unprivileged} --environment");
       $machine->mustSucceed("sleep 5; [ ! -f /tmp/wrapper.state ]");
       
@@ -157,6 +165,18 @@ makeTest {
       $machine->mustSucceed("[ \"\$(systemctl status disnix-\$(basename ${process_unprivileged}) | grep \"Active: active\")\" != \"\" ]");
       $machine->mustSucceed("[ \"\$(ps aux | grep ${process_unprivileged}/bin/loop | grep -v grep | grep unprivileged)\" != \"\" ]");
       
+      # Activate again. This test should succeed as the operation is idempotent.
+      $machine->mustSucceed("dysnomia --type process --operation activate --component ${process_unprivileged} --environment");
+      $machine->mustSucceed("sleep 5");
+      $machine->mustSucceed("[ \"\$(systemctl status disnix-\$(basename ${process_unprivileged}) | grep \"Active: active\")\" != \"\" ]");
+      $machine->mustSucceed("[ \"\$(ps aux | grep ${process_unprivileged}/bin/loop | grep -v grep | grep unprivileged)\" != \"\" ]");
+      
+      # Deactivate the process
+      $machine->mustSucceed("dysnomia --type process --operation deactivate --component ${process_unprivileged} --environment");
+      $machine->mustSucceed("sleep 5");
+      $machine->mustSucceed("[ \"\$(systemctl status disnix-\$(basename ${process_unprivileged}) | grep \"Active: inactive\")\" != \"\" ]");
+      
+      # Deactivate again. This test should succeed as the operation is idempotent.
       $machine->mustSucceed("dysnomia --type process --operation deactivate --component ${process_unprivileged} --environment");
       $machine->mustSucceed("sleep 5");
       $machine->mustSucceed("[ \"\$(systemctl status disnix-\$(basename ${process_unprivileged}) | grep \"Active: inactive\")\" != \"\" ]");
