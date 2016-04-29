d638cb9d3508141242e50157d5ecdc9507534c97
Sander van der Burg <svanderburg@gmail.com>
2016-04-29 18:43:18 +0000

    Refactor container generation functionality into a separate module

--- dysnomia-module.nix
+++ dysnomia-module.nix
@@ -110,32 +110,10 @@ in
       tarball = (import ./release.nix {}).tarball;
     });
     
-    dysnomia.containers = {
-      process = {};
-      wrapper = {};
-    }
-    // lib.optionalAttrs (config.services.httpd.enable) { apache-webapplication = {
-      documentRoot = config.services.httpd.documentRoot;
-    }; }
-    // lib.optionalAttrs (config.services.tomcat.axis2.enable) { axis2-webservice = {}; }
-    // lib.optionalAttrs (config.services.ejabberd.enable) { ejabberd-dump = {
-      ejabberdUser = config.services.ejabberd.user;
-    }; }
-    // lib.optionalAttrs (config.services.mysql.enable) { mysql-database = {
-      mysqlUsername = "root";
-      mysqlPassword = builtins.readFile (config.services.mysql.rootPassword);
-      mysqlPort = config.services.mysql.port;
-    }; }
-    // lib.optionalAttrs (config.services.postgresql.enable) { postgresql-database = {
-      postgresqlUsername = "root";
-    }; }
-    // lib.optionalAttrs (config.services.tomcat.enable) { tomcat-webapplication = {
-      tomcatPort = 8080;
-    }; }
-    // lib.optionalAttrs (config.services.mongodb.enable) { mongo-database = {}; }
-    // lib.optionalAttrs (config.services.svnserve.enable) { subversion-repository = {
-      svnBaseDir = config.services.svnserve.svnBaseDir;
-    }; };
+    dysnomia.containers = import ./nix/generate-containers.nix {
+      inherit config lib;
+      enableAuthentication = true;
+    };
     
     system.activationScripts.dysnomia = ''
       mkdir -p /etc/systemd-mutable/system
---
+++ nix/generate-containers.nix
@@ -0,0 +1,30 @@
+{config, lib, enableAuthentication}:
+
+{
+  process = {};
+  wrapper = {};
+}
+// lib.optionalAttrs (config.services.httpd.enable) { apache-webapplication = {
+  documentRoot = config.services.httpd.documentRoot;
+}; }
+// lib.optionalAttrs (config.services.tomcat.axis2.enable) { axis2-webservice = {}; }
+// lib.optionalAttrs (config.services.ejabberd.enable) { ejabberd-dump = {
+  ejabberdUser = config.services.ejabberd.user;
+}; }
+// lib.optionalAttrs (config.services.mysql.enable) { mysql-database = {
+    mysqlPort = config.services.mysql.port;
+  } // lib.optionalAttrs enableAuthentication {
+    mysqlUsername = "root";
+    mysqlPassword = builtins.readFile (config.services.mysql.rootPassword);
+  };
+}
+// lib.optionalAttrs (config.services.postgresql.enable && enableAuthentication) { postgresql-database = {
+  postgresqlUsername = "root";
+}; }
+// lib.optionalAttrs (config.services.tomcat.enable) { tomcat-webapplication = {
+  tomcatPort = 8080;
+}; }
+// lib.optionalAttrs (config.services.mongodb.enable) { mongo-database = {}; }
+// lib.optionalAttrs (config.services.svnserve.enable) { subversion-repository = {
+  svnBaseDir = config.services.svnserve.svnBaseDir;
+}; }
