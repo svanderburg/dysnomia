92c47553fa99f6102e4d19a3858f7d81646dd560
Sander van der Burg <svanderburg@gmail.com>
2019-09-08 16:59:59 +0200

    Modify Axis2 testcase to work with version 1.7.x

--- tests/axis2-webservice.nix
+++ tests/axis2-webservice.nix
@@ -13,11 +13,11 @@ with import "${nixpkgs}/nixos/lib/testing.nix" { system = builtins.currentSystem
 
 let
   # Test services
-  
+
   tomcat_webapplication = import ./deployment/tomcat-webapplication.nix {
     inherit stdenv jdk;
   };
-  
+
   axis2_webservice = import ./deployment/axis2-webservice.nix {
     inherit stdenv jdk;
   };
@@ -25,37 +25,37 @@ in
 makeTest {
   nodes = {
     machine = {config, pkgs, ...}:
-      
+
     {
       virtualisation.memorySize = 1024;
       virtualisation.diskSize = 4096;
-        
+
       services.tomcat.enable = true;
       services.tomcat.axis2.enable = true;
-        
+
       environment.systemPackages = [ dysnomia ];
     };
   };
-  
+
   testScript =
     ''
       startAll;
-      
+
       $machine->waitForJob("tomcat");
 
       # Test Axis2 web service script.
-      
+
       $machine->waitForFile("/var/tomcat/webapps/axis2");
       $machine->mustSucceed("dysnomia --type axis2-webservice --operation activate --component ${axis2_webservice} --environment");
       $machine->mustSucceed("sleep 10; curl --fail http://localhost:8080/axis2/services/Test/test"); # !!! We must wait a while to let it become active
-      
+
       # Activate again. This should succeed as the operation is idempotent
       $machine->mustSucceed("dysnomia --type axis2-webservice --operation activate --component ${axis2_webservice} --environment");
       $machine->mustSucceed("sleep 10; curl --fail http://localhost:8080/axis2/services/Test/test"); # !!! We must wait a while to let it become active
-      
+
       $machine->mustSucceed("dysnomia --type axis2-webservice --operation deactivate --component ${axis2_webservice} --environment");
       $machine->mustFail("sleep 10; curl --fail http://localhost:8080/axis2/services/Test/test"); # !!! We must wait a while to let it become inactive
-      
+
       # Deactivate again. This should succeed as the operation is idempotent
       $machine->mustSucceed("dysnomia --type axis2-webservice --operation deactivate --component ${axis2_webservice} --environment");
       $machine->mustFail("sleep 10; curl --fail http://localhost:8080/axis2/services/Test/test"); # !!! We must wait a while to let it become inactive
