168d4c3d03cc0247e9b32f1a44f030d58fdb5da9
Sander van der Burg <svanderburg@gmail.com>
2019-12-28 01:12:53 +0100

    Add cygrunsrv-service module

--- release.nix
+++ release.nix
@@ -1,5 +1,5 @@
 { nixpkgs ? <nixpkgs>
-, systems ? [ "i686-linux" "x86_64-linux" "x86_64-darwin" "x86_64-freebsd" ]
+, systems ? [ "i686-linux" "x86_64-linux" "x86_64-darwin" "x86_64-freebsd" "x86_64-cygwin" ]
 , dysnomia ? { outPath = ./.; rev = 1234; }
 , officialRelease ? false
 }:
